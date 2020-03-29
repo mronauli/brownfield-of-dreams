71151fa0a18573c469dcb4bbbc332a2962658a33
Maria Ronauli <mronauli@gmail.com>
2020-03-28 18:14:52 -0600

    add oauth test
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

---
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+context "as a logged in user" do
+  describe "when it connects to Github", :vcr do
+    it "can see its repository" do
+      user = create(:user)
+
+      visit '/'
+      click_on "Sign In"
+
+      fill_in 'session[email]', with: user.email
+      fill_in 'session[password]', with: user.password
+
+      click_on 'Log In'
+
+      visit "/dashboard"
+      expect(page).to_not have_content("battleship")
+      mock_auth_hash
+      click_link "Connect to Github"
+      expect(current_path).to eq("/dashboard")
+      expect(page).to have_content("battleship")
+    end
+  end
+end
