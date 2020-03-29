7adffaf9b3ca3c478a3ae888e4abfa28445e134d
Alex Gallant <agallant1201@gmail.com>
2020-03-29 16:50:44 -0600

    Add route to add friend
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,9 @@ Rails.application.routes.draw do
   get "/auth/github", as: "github_login"
   get "/auth/github/callback", to: "sessions#update"
 
+  post "/friendships/create", to: "friendships#create"
+  # resources :friendships, only:[:create]
+
   # Is this being used?
   get '/video', to: 'video#show'
 
