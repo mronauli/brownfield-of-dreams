b46c9bbc266598932a3d605a5289da5223b19212
Maria Ronauli <mronauli@gmail.com>
2020-03-28 18:17:18 -0600

    add config for omniauth
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  #add github as a provider
+  provider :github, ENV['GITHUB_ID_TEST'], ENV['GITHUB_SECRET_TEST']
 end
