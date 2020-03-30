69955a57e59de053825da1894dfec1153c19937c
Alex Gallant <agallant1201@gmail.com>
2020-03-30 14:37:50 -0600

    Add github_id to users table
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_205228) do
+ActiveRecord::Schema.define(version: 2020_03_30_194125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_205228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.string "github_id"
     t.index ["email"], name: "index_users_on_email"
   end
 
