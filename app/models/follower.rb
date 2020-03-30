b60ccbea6346720b0180965a0f8632fe80c09871
Alex Gallant <agallant1201@gmail.com>
2020-03-30 14:39:03 -0600

    Add github id as attribute for both the following and follower poros
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,8 +1,15 @@
 class Follower
-  attr_reader :login, :html_url
+  attr_reader :login, :html_url, :github_id
 
   def initialize(attributes)
     @login = attributes[:login]
     @html_url = attributes[:html_url]
+    @github_id = attributes[:id]
+  end
+
+  def find_by_id
+    # require "pry"; binding.pry
+    # User.where(github_id: id)
+    User.exists?(github_id: github_id)
   end
 end
--- app/models/following.rb
+++ app/models/following.rb
@@ -1,8 +1,9 @@
 class Following
-  attr_reader :login, :html_url
+  attr_reader :login, :html_url, :github_id
 
   def initialize(attributes)
     @login = attributes[:login]
     @html_url = attributes[:html_url]
+    @github_id = attributes[:id]
   end
 end
