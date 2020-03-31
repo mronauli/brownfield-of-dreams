e180417295b049da92bac1b9890378531fb3f761
Alex Gallant <agallant1201@gmail.com>
2020-03-30 18:41:19 -0600

    Add github id for user
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "As a user I can add friends who are in the DB but not those who are not" , :vcr do
   before :each do
     alex = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_2'])
-    maria = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_1'])
+    maria = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_1'], github_id: "55954962")
     bob = User.new(email: "example@email.com",
                    first_name: "Bob",
                    last_name: "Roberts",
@@ -15,27 +15,27 @@ RSpec.describe "As a user I can add friends who are in the DB but not those who 
 
   it "should have links to add friends next to followers/followings" do
     visit "/dashboard"
-    # save_and_open_page
+
     within "#github_section" do
-      expect(page).to have_content("Friends:")
+     expect(page).to have_content("Friends:")
     end
 
     within "#followers" do
-      expect(page).to have_button("Add as Friend", count: 2)
+      expect(page).to have_button("Add as Friend", count: 1)
     end
-    save_and_open_page
+
     within "#following" do
       expect(page).to have_button("Add as Friend", count: 1)
     end
 
-    # within "#following" do
-    #   click_on("Add as Friend")
-    # end
+    within "#followers" do
+      click_on("Add as Friend")
+    end
+
+    expect(current_path).to eq(dashboard_path)
+
+    within "#friends" do
+      expect(page).to have_content("baloo")
+    end
   end
-    #
-    # expect(current_path).to eq(dashboard_path)
-    #
-    # within "#friends" do
-    #   expect(page).to have_content("mronauli")
-    # end
 end
