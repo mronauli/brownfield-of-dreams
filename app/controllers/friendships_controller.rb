cbed39cd5f5056c1ebed91913991726bb02a3f09
Alex Gallant <agallant1201@gmail.com>
2020-03-30 18:43:25 -0600

    Update create action in friendships controller
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
-    require "pry"; binding.pry
-    if params.include?(:friend_id)
-      @new_friendships = Friendship.create_reciprocal_for_ids(current_user_id, params[:friend_id])
-    else
-      params[:user][:friend_ids].each do |friend_id|
-        require "pry"; binding.pry
-        @new_friendships = Friendship.create_reciprocal_for_ids(current_user_id, friend_id)
-      end
-    end
+    friend = User.find_by_github_id(params[:github_id])
+    Friendship.create!(user: current_user, friend: friend)
     redirect_to dashboard_path
   end
 
