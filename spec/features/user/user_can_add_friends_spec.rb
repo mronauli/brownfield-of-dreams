2dc528d301a6f88a4838ece61fc46d1950933e9a
Maria Ronauli <mronauli@gmail.com>
2020-03-30 19:39:59 -0600

    edit spec for adding friend
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "As a user I can add friends who are in the DB but not those who are not" , :vcr do
   before :each do
-    alex = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_2'])
-    maria = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_1'], github_id: "55954962")
+    alex = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_2'], github_id: "21334556")
+    maria = create(:user, token: ENV['GITHUB_ACCESS_TOKEN_1'], github_id: "55954962", first_name: "Maria", last_name: "Ronauli")
     bob = User.new(email: "example@email.com",
                    first_name: "Bob",
                    last_name: "Roberts",
@@ -11,11 +11,10 @@ RSpec.describe "As a user I can add friends who are in the DB but not those who 
                    role: "default")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(alex)
+    visit "/dashboard"
   end
 
-  it "should have links to add friends next to followers/followings" do
-    visit "/dashboard"
-
+  it "should have links to add friends next to followers" do
     within "#github_section" do
      expect(page).to have_content("Friends:")
     end
@@ -35,7 +34,23 @@ RSpec.describe "As a user I can add friends who are in the DB but not those who 
     expect(current_path).to eq(dashboard_path)
 
     within "#friends" do
-      expect(page).to have_content("baloo")
+      expect(page).to have_content("Maria Ronauli")
+    end
+  end
+
+  it "can add friends from following section" do
+    within "#following" do
+      expect(page).to have_button("Add as Friend", count: 1)
+    end
+
+    within "#following" do
+      click_on("Add as Friend")
+    end
+
+    expect(current_path).to eq(dashboard_path)
+
+    within "#friends" do
+      expect(page).to have_content("Maria Ronauli")
     end
   end
 end
