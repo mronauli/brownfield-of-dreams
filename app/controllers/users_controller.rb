52c238f8bb6c2fe2e5d4c84d6a91a9fd75644020
Alex Gallant <agallant1201@gmail.com>
2020-03-31 19:03:01 -0600

    Add email send function when user is created
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @github_info = GithubSearchFacade.new(current_user.token)
     @videos = UserVideo.order_bookmarked(current_user.id)
@@ -9,13 +10,26 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.create(user_params)
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver
+      session[:user_id] = @user.id
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
       render :new
+    end
+  end
+
+  def confirm_email
+    @user = User.find_by_confirm_token(params[:id])
+    if @user
+      @user.email_activate
+      flash[:success] = "Welcome! Your email has been confirmed."
+      redirect_to signin_url
+    else
+      flash[:error] = "Sorry. User does not exist"
+      redirect_to root_url
     end
   end
 
