fa61c5d636aa2b5c623af55c813c34d1f563b1e9
Alex Gallant <agallant1201@gmail.com>
2020-03-31 19:01:40 -0600

    Add method to check if email was confirmed
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_create :confirmation_token
+
   has_many :user_videos
   has_many :videos, through: :user_videos
 
@@ -10,4 +12,18 @@ class User < ApplicationRecord
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
+
+  def email_activate
+    self.email_confirmed = true
+    self.confirm_token = nil
+    save!(validate: false)
+  end
+
+  private
+
+  def confirmation_token
+    if self.confirm_token.blank?
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+    end
+  end
 end
