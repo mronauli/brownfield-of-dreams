a684641e79380b287274358728688ec8db290644
Maria Ronauli <mronauli@gmail.com>
2020-04-01 16:19:18 -0600

    add invites controller wip
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,7 +3,9 @@ class InvitesController < ApplicationController
 
   def create
     service = GithubService.new(current_user.token)
+    #current_user, email
     service.get_user(params[:github_handle])
+    require "pry"; binding.pry
     redirect_to dashboard_path
   end
 end
