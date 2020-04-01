001a300c0c0afed5c3af3c86e9dd0f5495424dbb
Maria Ronauli <mronauli@gmail.com>
2020-04-01 14:34:59 -0600

    change hash format
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
-
  def registration_confirmation(user)
     @user = user
-    mail(:to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>", :subject => "Registration Confirmation")
+    mail(to: "#{@user.first_name} #{@user.last_name} <#{@user.email}>", subject: "Registration Confirmation")
  end
 end
