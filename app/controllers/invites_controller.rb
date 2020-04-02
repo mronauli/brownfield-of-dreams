2c8ea10923e641a100a34bd0384977c679ff5daf
Maria Ronauli <mronauli@gmail.com>
2020-04-01 18:25:47 -0600

    add invites controller to send email invites
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,10 +2,16 @@ class InvitesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
+    handle = params[:github_handle]
     service = GithubService.new(current_user.token)
-    #current_user, email
-    service.get_user(params[:github_handle])
-    require "pry"; binding.pry
+    data = service.get_github_user_data(handle)
+    github_friend = GithubUser.new(data)
+    if !data[:email].nil?
+      FriendInviteMailer.github_invite(current_user, github_friend).deliver
+      flash[:success] = "Successfully sent invite!"
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+    end
     redirect_to dashboard_path
   end
 end
