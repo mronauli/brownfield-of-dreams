38ae4106d8689949895036e071e4762557a49a61
Maria Ronauli <mronauli@gmail.com>
2020-04-01 23:33:40 -0600

    add spec for new tutorial
    Co-Authored-By: Alex Gallant <52808022+agallant121@users.noreply.github.com>

---
+++ spec/features/admin/admin_can_add_new_tutorial_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "as an admin user", :vcr do
+  before(:each) do
+    admin = User.create(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password:  "password", role: :admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  end
+  it "can create new tutorials" do
+    visit new_admin_tutorial_path
+
+    title = "Wonders of the World"
+    description = "Super meaningful tutorial about one of the wonders of the world..."
+    thumbnail = "https://i.ytimg.com/vi/VBmMU_iwe6U/hqdefault.jpg"
+
+    fill_in "tutorial[title]", with: title
+    fill_in "tutorial[description]", with: description
+    fill_in "tutorial[thumbnail]", with: thumbnail
+
+    click_on "Save"
+    expect(page).to have_content("Tutorial created!")
+    expect(current_path).to eq(tutorial_path(Tutorial.last))
+  end
+
+  it "cannot create new tutorials if fields are missing" do
+    visit new_admin_tutorial_path
+
+    title = "Wonders of the World"
+    description = "Super meaningful tutorial about the wonders of the world"
+    thumbnail = ""
+
+    fill_in "tutorial[title]", with: title
+    fill_in "tutorial[description]", with: description
+    fill_in "tutorial[thumbnail]", with: thumbnail
+
+    click_on "Save"
+
+    expect(page).to have_content("Please fill in all fields.")
+    expect(current_path).to eq(new_admin_tutorial_path)
+  end
+end
