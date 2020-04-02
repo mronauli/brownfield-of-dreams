5b70c0731a4bd12e9da8d55b5227c8dd89a8e443
Alex Gallant <agallant1201@gmail.com>
2020-04-01 22:09:26 -0600

    Add spec to make sure only logged in user can bookmark videos
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>

--- spec/features/vistors/visitor_is_asked_to_log_in_when_bookmarking_spec.rb
+++ spec/features/vistors/visitor_is_asked_to_log_in_when_bookmarking_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'visitor visits video show page' do
-  it 'clicks on the bookmark page and is sent to the log in page' do
+  it 'cannot click on the bookmark page and is asked to log in' do
     tutorial = create(:tutorial)
     video = create(:video, tutorial_id: tutorial.id)
 
     visit tutorial_path(tutorial)
 
-    click_on 'Bookmark'
-
+    expect(page).to have_content("User must login to bookmark videos.")
+    click_link "Login"
     expect(current_path).to eq(login_path)
   end
 end
