04a8249528e622fac2f09b04fa90897175d2f2c7
Maria Ronauli <mronauli@gmail.com>
2020-04-02 11:34:08 -0600

    edit production for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do # rubocop:todo Metrics/BlockLength
    config.action_mailer.smtp_settings = {
      address:              'smtp.sendgrid.net',
      port:                 '587',
-     domain:               'example.com',
+     domain:               'herokuapp.com',
      user_name:            ENV["SENDGRID_USERNAME"],
      password:             ENV["SENDGRID_PASSWORD"],
      authentication:       'plain',
